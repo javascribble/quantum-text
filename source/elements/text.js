3489409486c65b52021d7c5b206744ea35a5b1d5
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-03-04 10:07:45 -0500

    merge repositories

--- source/elements/text.js
+++ source/elements/text.js
@@ -1,9 +1,5 @@
-import html from '../templates/text.js';
-
 export class Text extends Quantum {
     constructor() {
         super();
     }
 }
-
-Text.define('quantum-text', html);
--- source/main.js
+++ source/main.js
@@ -1,1 +1,4 @@
-import './elements/text.js';
+import { Text } from './elements/text.js';
+import text from './templates/text.js';
+
+Text.define('quantum-text', text);
