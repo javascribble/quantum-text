35490a12d6e50a4821e88892b106a17f59b1d32a
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-07-12 16:08:32 -0400

    update build

--- source/elements/text.js
+++ source/elements/text.js
@@ -1,5 +1,9 @@
+import text from '../templates/text.js';
+
 export class Text extends Quantum {
     constructor() {
         super();
     }
 }
+
+Text.define('quantum-text', text);
--- source/main.js
+++
@@ -1,4 +0,0 @@
-import { Text } from './elements/text.js';
-import text from './templates/text.js';
-
-Text.define('quantum-text', text);
